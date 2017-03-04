4d9b937503f7d05e52c06a0cff9e0c317d5b20c1
rurik <urrast@yandex.ru>
2017-03-04 23:48:41 +0400

    list of clients with add button

--- ui/app/app.component.ts
+++ ui/app/app.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+
+export class ApiClient {
+    id: String;
+    privateKey: String;
+    allowedUrls: String[];
+
+
+    constructor(id: String, privateKey: String, allowedUrls?: String[]) {
+        this.id = id;
+        this.privateKey = privateKey;
+        this.allowedUrls = allowedUrls;
+    }
+
+
+}
 
 @Component({
     selector: 'my-app',
-    template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
+    template: `<div class="page-header">
+        <h1>Клиенты Api</h1>
+    </div>
+    <div class="panel">
+        <div class="form-inline">
+            <div class="form-group">
+                <div class="col-md-8">
+                    <input class="form-control" [(ngModel)]="id" placeholder = "Id" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6">
+                    <input  class="form-control" [(ngModel)]="privateKey" placeholder="Секретный ключ" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-offset-2 col-md-8">
+                    <button class="btn btn-default" (click)="addApiClient(id, privateKey)">Добавить</button>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Секретный ключ</th>
+                    <th>Список разрешенных адресов</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr *ngFor="let client of clients">
+                    <td>{{client.id}}</td>
+                    <td>{{client.privateKey}}</td>
+                    <td>{{client.allowedUrls}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`
 })
 export class AppComponent {
-    name = '';
+    clients: ApiClient[] = [
+        {id: "id1", privateKey: "wsewdwe", allowedUrls: ["/api1", "/api2"]},
+        {id: "id2", privateKey: "wsewdwe", allowedUrls: ["/api1", "/api2"]}
+    ];
+
+    addApiClient(id: string, privateKey: string): void {
+        if (id == null || id == undefined || id.trim() == "")
+            return;
+        if (privateKey == null || privateKey == undefined)
+            return;
+        this.clients.push(new ApiClient(id, privateKey));
+    }
+
+
 }
